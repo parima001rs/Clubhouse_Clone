3d8890cf2218fd8b25b02d90338cc2c48eaa673d
Parima Kashyap <parima001rs@gmail.com>
2021-11-18 22:24:32 +0530

    modified database

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
   <React.StrictMode>
